5dc2ed2fa67d168dad0953ecc61ce9e6bec62a94
aaronl647 <AaronLei@Aarons-MacBook-Pro.local>
2020-03-03 13:25:08 -0500

    added an randomizer function

--- js/main.js
+++ js/main.js
@@ -6,20 +6,20 @@ const crino = [
     { name: 'ditto-suits', desc: 'https://imgur.com/OR5hxWC', img: 'https://imgur.com/HlD6YGe', id: 2 },
     { name: 'empres-eugenie', desc: 'https://imgur.com/0063iSI', img: 'https://imgur.com/vAtBysz', id: 3 },
     { name: 'frock-suit', desc: 'https://imgur.com/c2nguEB', img: 'https://imgur.com/IvFiNfO', id: 4 },
-    { name: 'knickerbockers', desc: 'https://imgur.com/2WL46Eg', img: 'https://imgur.com/2csnAAs', id: 5},
+    { name: 'knickerbockers', desc: 'https://imgur.com/2WL46Eg', img: 'https://imgur.com/2csnAAs', id: 5 },
     { name: 'morning-coat', desc: 'https://imgur.com/cfdCL5Q', img: 'https://imgur.com/05nX2lo', id: 6 },
     { name: 'sack-jacket', desc: 'https://imgur.com/cU1GwWg', img: 'https://imgur.com/9DRsPAs', id: 7 },
     { name: 'trousers', desc: 'https://imgur.com/ceiVRd3', img: 'https://imgur.com/z16QVgF', id: 8 },
     { name: 'wm-under', desc: 'https://imgur.com/QVplrkL', img: 'https://imgur.com/8gylj7R', id: 9 },
     { name: 'womens-dress', desc: 'https://imgur.com/NA93Mfh', img: 'https://imgur.com/TTeJQTr', id: 10 },
-]
+];
 /*----- app's state (variables) -----*/
 
 /*----- cached element references -----*/
 let box = document.getElementById("mainScreen")
 let modal = document.getElementById("simpleModal")
-let modalBtn = document.getElementById("mainScreen")
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
+//let modalBtn = document.getElementById("mainScreen")
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
 let modalContent = document.getElementsByClassName('modal-content')[0]
 var targetDiv = document.getElementsByClassName('modal-content');
 
@@ -29,17 +29,22 @@ modalContent.addEventListener('click', cardTiles);
 closeBtn.addEventListener('click', closeModal);
 
 /*----- functions -----*/
-function mainScreen(event) {
+function mainScreen() {
     console.log("o hai dere")
 }
 //-------------------------------------------------------------------------------------  
 function openModal(event) {
+    //categorySelector();
     console.log(event.target)
     modal.style.display = "block";
 }
 function closeModal() {
     modal.style.display = "none";
 }
+
+// function categorySelector(event){
+//     console.log(`You selected ${box.target}`);
+// }
 //-------------------------------------------------------------------------------------  
 function showIt(element) {
     var parent = element.parentNode;
@@ -47,57 +52,56 @@ function showIt(element) {
 }
 function cardTiles(showIt) {
     //Shows which item on the grid has been clicked
-    let cards = splitPairs(crino);
-    let cardsShuffled = shuffleCards(cards)
     let tile = showIt.target
     console.log(tile)
-    //console.log(cards)
-    console.log(cardsShuffled)
-    cardsShuffled.forEach((card, i) => {
-        console.log(card)
-        //tile.innerHTML = `<img id='num${cardsShuffled.i}' src='${cardsShuffled.val}>`
-    })
+    shuffleCards();
+    tile.innerHTML = `<img id='num${}' src='${}>`
 }
 
-//this function takes the original array and puts
-//descriptions and index and images and indexes together
-// function splitPairs(crino) {
-//     let cardsArray = []
-//     crino.forEach((pair, i) => {
-//         )
-//     });
-//     return cardsArray;
-// }
-function shuffleCards(crino) {
-    let cardsArray = []
-    crino.forEach((pair)=>{
-        cardsArray.push({ val: pair.desc, id: pair.id })
-        cardsArray.push({ val: pair.img, id: pair.id })
+function shuffleCards() {
+    let splitArray = [];
+    let shuffled = [];
+    crino.forEach(pair => {
+        splitArray.push({ val: pair.desc, id: pair.id }),
+            splitArray.push({ val: pair.img, id: pair.id })
     })
-    return cardsArray
+    for (let i = 0; i < splitArray.length; i++) {
+        let rdmIdx = Math.floor(Math.random() * splitArray.length);
+        let returnedCardArray = splitArray.splice(rdmIdx, 1);
+        let card = returnedCardArray[0];
+        shuffled.push(card)
+    }
+    shuffled.forEach(content => console.log(content))
 }
-//     for (let i = 0; i < crino.length; i++) {
-//         let randomIndex = Math.floor(Math.random() * crino.length);
-//         let returnedArray = arrayCopy.splice(randomIndex, 1);
-//         let card = returnedArray[0]
-//         shuffled.push(card);
-//     }
-//     return shuffled;
-// }
-console.log(shuffleCards())
-// let cards = splitPairs(crino);
-// let cardsShuffled = shuffleCards(cards)
+
+function randomizeCards() {
+    let randomCards = [];
+    getCards()
+}
+
 
 mainScreen()
-
+// randomizeCards()
 /*---------------------Test Area---------------------*/
-//-------------------------------------------------------------------------------------  
 
-//---------------------------------------------------------------------------------------    
 
-//------------------------------------------------------------------------------------- 
 
-//--------------------------------------------------------------------------------------
+//   function shuffleCards(cards) {
+//     let cardsCopy = [].concat(cards);
+//     let shuffled = [];
+//     for (let i = 0; i < cards.length; i++) {
+//       let randomIndex = Math.floor(Math.random() * cardsCopy.length);
+//       let returnedCardArray = cardsCopy.splice(randomIndex, 1);
+//       let card = returnedCardArray[0];
+//       shuffled.push(card);
+//     }
+//     return shuffled;
+//   }
+//   let cards = splitPairs(pairs);
+//   let shuffledCards = shuffleCards(cards);
+//   console.log(cards);
+//   console.log(shuffledCards);
+
 /*---------------------Test Area---------------------*/
 
 
@@ -114,6 +118,11 @@ mainScreen()
           or an image
     - The player has to match all the tiles
         when the player guesses wrong, the tile
+
+                    //Current objectives
+//1. When a tile is clicked, either a description or image appears in the tile
+    // Make sure that the tiles DO NOT repeat themselves
+//2. If the tiles do not match, they must flip back to the question marks
 
     -----------------Stretch Goals-----------------
     -
